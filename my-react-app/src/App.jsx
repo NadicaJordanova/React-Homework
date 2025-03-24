7e27cc710fddb52cce7c78fb5a5da6d43a388b20
NadicaJordanova <nadicajordanova3@gmail.com>
2025-03-24 19:01:53 +0100

    chages

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+import Header from './Header.jsx'
+import Menu from './Menu.jsx'
+import Footer from './footer.jsx'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
