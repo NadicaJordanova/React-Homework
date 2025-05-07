f0bd24c4eb4a9607bf8ee6c966db5bbfb361f465
NadicaJordanova <nadicajordanova3@gmail.com>
2025-05-07 22:12:23 +0200

    typo fix

--- Vezba/my-react-app/src/components/Home/Home.jsx
+++ Vezba/my-react-app/src/components/Home/Home.jsx
@@ -2,32 +2,45 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCharacters } from "../../actions/charachterActions";
 import CharacterCard from "../CharachterCard/CharachterCard";
+import { searchCharacterDetail } from "../../actions/searchCharachter";
 
 function Home({searchCharachter}) {
   const { charachters, error, loading } = useSelector(
     (state) => state.charachters
   );
-   const [search, setSearch] = useState('')
-  const [searchedChar, setSearchedChar] = useState('')
+
+  const[search, setSearch] = useState("")
+
   const handleChange = (e) => {
     setSearch(e.target.value)
   }
-
- 
-
+   console.log(search)
+   
+    
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCharacters());
+
   }, [dispatch]);
+  const filteredCharacters = charachters?.results?.filter((char) =>
+    char.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
   return (
     
     <div>
-       <input type="text" placeholder="search" value={searchTerm} onChange={handleChange}/>
-      {charachters?.results?.map((charachter) => (
+       <input type="text" placeholder="search" value={search} onChange={handleChange}/>
+       {search ? filteredCharacters.map(((charachter) => (
+        <CharacterCard
+          name={charachter.name}
+          img={charachter.image}
+          status={charachter.status}
+          id={charachter.id}
+        />
+      ))) : charachters?.results?.map((charachter) => (
         <CharacterCard
           name={charachter.name}
           img={charachter.image}
@@ -35,6 +48,7 @@ function Home({searchCharachter}) {
           id={charachter.id}
         />
       ))}
+      {}
     </div>
   );
 }
